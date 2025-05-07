a4d3c283ad985801a2423aca77e504a24ac083cc
Marc LEPAGNEUL <marc@lepagneul.fr>
2025-05-07 17:37:48 +0200

    CPP Difference of squares

---
+++ cpp/difference-of-squares/CMakeFiles/difference-of-squares.dir/difference_of_squares.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/difference-of-squares.dir/difference_of_squares.cpp.o: \
+ /home/stratus/dev/exercism/cpp/difference-of-squares/difference_of_squares.cpp \
+ /usr/include/stdc-predef.h \
+ /home/stratus/dev/exercism/cpp/difference-of-squares/difference_of_squares.h
