d433f915da4fb5488c0ee8a1bf554ba4b9f8abca
Marc LEPAGNEUL <marc@lepagneul.fr>
2025-05-07 17:30:16 +0200

    CPP Election day

---
+++ cpp/election-day/CMakeFiles/election-day.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/stratus/dev/exercism/cpp/election-day/election_day_test.cpp" "CMakeFiles/election-day.dir/election_day_test.cpp.o" "gcc" "CMakeFiles/election-day.dir/election_day_test.cpp.o.d"
+  "/home/stratus/dev/exercism/cpp/election-day/test/tests-main.cpp" "CMakeFiles/election-day.dir/test/tests-main.cpp.o" "gcc" "CMakeFiles/election-day.dir/test/tests-main.cpp.o.d"
+  "" "election-day" "gcc" "CMakeFiles/election-day.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
